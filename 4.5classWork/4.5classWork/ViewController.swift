325223cdb036e9dd307277b8fd823d6d02047eab
ismailchinloda <113357847+ismailchinloda@users.noreply.github.com>
2023-02-08 19:23:12 +0600

    Add files via upload

---
+++ 4.5classWork/4.5classWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4.5classWork
+//
+//  Created by Ismail Chinloda on 8/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
